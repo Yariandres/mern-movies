68be9d69dd5aa52ebe4e297e527ecff8897811b4
Yari Andres <yari.andres@gmail.com>
2020-05-25 12:51:33 +0200

    adding client

--- routes/movie-router.js
+++ routes/movie-router.js
@@ -5,7 +5,6 @@ const MovieCtrl = require('../controllers/movie-controller');
 const router = express.Router();
 
 router.post('/movie', MovieCtrl.createMovie);
-
 router.delete('/movie/:id', MovieCtrl.deleteMovie);
 router.get('/movie/:id', MovieCtrl.getMovieById);
 router.get('/movies', MovieCtrl.getMovies);
